c5b3187b6746f4b8a8b26195f6ea2c80a8ab1dbf
Mahesh Amruthaluri <amahesh2811@gmail.com>
2017-06-28 13:46:17 -0400

    Add files via upload

--- Algorithms/warmup/MinMax.java
+++ Algorithms/warmup/MinMax.java
@@ -4,7 +4,9 @@ public class MinMax {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int[] arr = new int[5];
-        int sum = 0;
+        long sum = 0;
+        long max = Long.MIN_VALUE;
+        long min = Long.MAX_VALUE;
         for(int arr_i=0; arr_i < 5; arr_i++){
             arr[arr_i] = in.nextInt();
         }
@@ -15,9 +17,16 @@ public class MinMax {
         		}
         		
         	}
-        	System.out.println(sum);
+        	if(max < sum){
+        		max = sum;
+            	//System.out.println(sum);
+        	}
+        	if(min > sum){
+        		min = sum;
+        	}
         	sum = 0;
         }
+    	System.out.println(min+" "+max);
     }
 
 }
