24e1a3c71e972d20285b15d0f5cd013e0799efd6
Mahesh Amruthaluri <amahesh2811@gmail.com>
2017-06-30 13:28:28 -0400

    Add files via upload

---
+++ Algorithms/Implementation/BreakingRecords.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class BreakingRecords {
+	
+	static void getRecord(int[] s){
+		int currentRecord = s[0];
+		int bestRecord = 0;
+		int worstRecord = 0;
+		for(int i=0; i<s.length; i++){
+			if(s[i] > currentRecord){
+				currentRecord = s[i];
+				bestRecord++;
+			}
+		}
+		currentRecord = s[0];
+		for(int j=0; j<s.length; j++){
+			if(s[j] < currentRecord){
+				currentRecord = s[j];
+				worstRecord++;
+			}
+		}
+		System.out.println(bestRecord+" "+worstRecord);
+		
+	}
+	
+	public static void main(String[] args) {
+		BreakingRecords records = new BreakingRecords();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] s = new int[n];
+        for(int s_i=0; s_i < n; s_i++){
+            s[s_i] = in.nextInt();
+        }
+        records.getRecord(s);
+        //System.out.println(result);
+        
+
+}
+}
