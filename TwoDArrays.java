bf9a33ace850ac23f4ae1fcacc4f833e5ed55ab9
Mahesh Amruthaluri <amahesh2811@gmail.com>
2017-08-23 20:17:36 -0400

    Update TwoDArrays.java

--- TwoDArrays.java
+++ TwoDArrays.java
@@ -16,7 +16,7 @@ public class HackerRank11 {
 		for(int i = 0; i < 4; i++){
 			for(int j = 0; j < 4; j++){
 				if(i + 2 < 4 && j + 2 < 4){
-				sum =sum + (arr[i][j] + arr[i][j + 1] +arr[i][j + 2]);
+				sum = sum + (arr[i][j] + arr[i][j + 1] +arr[i][j + 2]);
 				sum = sum + (arr[i + 1][j + 1]);
 				sum = sum + (arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j +2]);
 				if(max < sum){
